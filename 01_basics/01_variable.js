91f8dd2f9c20ecf60a12e219db8155d8f7cb62a9
Muhammad Shoaib zain <48830453+shoaibzain@users.noreply.github.com>
2024-02-21 11:03:36 +0000

    Variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "test@google.com"
+var accountPassword = "12345"
+accountCity = "test"
+
+// accountId = 2 // not allowed
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "test2"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
